61e3bcaf6b33d37ab7b004121de817b9486f65a6
jakobmanthey <j.manthey@uke.de>
2026-04-15 16:00:03 +0200

    first version with map and coverage plots

---
+++ 1_prepare data.R
@@ -0,0 +1,191 @@
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+# PROJECT TITLE:  MLDA paper
+# CODE AUTHOR:    JAKOB
+# DATE STARTED:   260415
+
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+# 0) ESSENTIALS
+# ______________________________________________________________________________________________________________________
+
+# clean workspace
+rm(list=ls())
+
+packages <- c("data.table", "ggplot2", "ggthemes", "openxlsx"
+              #"Hmisc", "tidyr", "nnet",
+              #"tableone", "knitr", "kableExtra",
+              #"survey"
+              )
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+# Load packages
+invisible(lapply(packages, library, character.only = TRUE))
+
+# current date:
+DATE <- format(Sys.Date(), "%y%m%d")
+
+# themes and options
+theme_set( theme_gdocs() + #base_family = "Aptos"
+             theme( panel.background = element_rect(fill = "white") )
+)
+options(scipen = 999)
+
+# fonts:
+extrafont::loadfonts(device = "win")
+
+# viridis colors: https://waldyrious.net/viridis-palette-generator/
+vcol3 <- viridis::viridis(3)
+vcol4 <- viridis::viridis(4)
+vcol5 <- viridis::viridis(5)
+vcol6 <- viridis::viridis(6)
+
+# plasma colors: https://waldyrious.net/viridis-palette-generator/
+pcol <- viridis::plasma(1)
+
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+# 1) LOAD DATA
+# ______________________________________________________________________________________________________________________
+
+##  MLDA data
+# -------------------------------------------------------
+
+path <- file.path("input", "2024-05-17_Alcohol Policy Scale_Age limits_All countries.xlsx")
+input1 <- data.table(read.xlsx(xlsxFile = path))
+
+nrow(input1) # 73
+
+
+##  Population data
+# -------------------------------------------------------
+
+##  up to 2023
+##  accessed on 15 April 2026 from UN: https://population.un.org/wpp/assets/Excel%20Files/1_Indicator%20(Standard)/EXCEL_FILES/2_Population/WPP2024_POP_F01_1_POPULATION_SINGLE_AGE_BOTH_SEXES.xlsx
+##  standard projection / population / single ages / both sexes:
+##  "Total population (both sexes combined) by single age. De facto population as of 1 July of the year indicated classified by single age (0, 1, 2, ..., 99, 100+). Data are presented in thousands."
+
+path <- file.path("input", "WPP2024_POP_F01_1_POPULATION_SINGLE_AGE_BOTH_SEXES.xlsx")
+input2 <- data.table(read.xlsx(xlsxFile = path, startRow = 17, sheet = "Estimates"))
+
+nrow(input2) # 21983
+
+##  projection after 2024:
+##  accessed on 15 April 2026 from UN: https://population.un.org/wpp/assets/Excel%20Files/2_Indicators%20(Probabilistic)/EXCEL_FILES/2_Population/UN_PPP2024_Output_PopTot.xlsx
+##  probabilistic projection / population / probabilistic population both sexes
+
+path <- file.path("input", "UN_PPP2024_Output_PopulationBySingleAge_BothSexes.xlsx")
+input3 <- data.table(read.xlsx(xlsxFile = path, startRow = 17, sheet = "Median"))
+
+nrow(input3) # 22950
+
+
+
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+## 2) PREPARE DATA
+# ______________________________________________________________________________________________________________________
+
+## 2.1) MLDA
+#-------------------------------------------------------
+
+mldadat <- copy(input1)
+mldadat <- mldadat[1:62]
+mldadat$X1 <- mldadat$X35 <- mldadat$X36 <- NULL
+
+mldadat[, country := zoo::na.locf(COUNTRY)]
+mldadat[,.(country,COUNTRY)]
+mldadat$COUNTRY <- NULL
+
+mldadat[, premise := ifelse(Legal.age.limit %like% "On-premise", "on", "off")]
+mldadat$Legal.age.limit <- NULL
+
+mldadat <- melt(mldadat, id.vars = c("country","premise"), variable.name = "year", value.name = "MLDA")
+mldadat$year <- as.numeric(as.character(mldadat$year))
+
+mldadat <- mldadat[year>=2000]
+
+## 2.2) Population
+#-------------------------------------------------------
+
+##  until 2023
+popdat1  <- copy(input2)
+
+keep    <- names(popdat1)[names(popdat1) %like% "^[0-9]|Year|Region"]
+popdat1  <- popdat1[,.SD, .SDcols = keep] ; rm(keep)
+popdat1[, country := `Region,.subregion,.country.or.area.*`]
+popdat1[, year := as.numeric(Year)]
+popdat1$Year <- popdat1$`Region,.subregion,.country.or.area.*`  <- NULL
+
+popdat1 <- melt(popdat1, id.vars = c("country","year"), variable.name = "age", value.name = "pop")
+
+##  from 2024
+popdat2  <- copy(input3)
+
+keep    <- names(popdat2)[names(popdat2) %like% "^[0-9]|Year|Region"]
+popdat2  <- popdat2[,.SD, .SDcols = keep] ; rm(keep)
+popdat2[, country := `Region,.subregion,.country.or.area.*`]
+popdat2[, year := as.numeric(Year)]
+popdat2$Year <- popdat2$`Region,.subregion,.country.or.area.*`  <- NULL
+
+popdat2 <- melt(popdat2, id.vars = c("country","year"), variable.name = "age", value.name = "pop")
+
+##  all years
+popdat <- rbind(popdat1,popdat2)[order(country,year,age)]
+popdat$pop <- as.numeric(popdat$pop)
+popdat$pop <- popdat$pop/1000
+
+popdat <- popdat[age %in% c(16,17,18,19,20,21) & year %in% c(2000:2025)]
+popdat[, pop_all := sum(pop), by = .(country,year)]
+
+ggplot(data = popdat[country %like% "Austria|Germany|Finland|Italy"], aes(x = year, y = pop_all, color = country)) + 
+  geom_line() + 
+  geom_vline(xintercept = 2024)
+
+
+## 2.3) Combine
+#-------------------------------------------------------
+
+mnames <- unique(mldadat$country)
+pnames <- unique(popdat$country)
+
+mnames[!mnames %in% pnames] # Only Czech Republic is missing
+pnames[pnames %like% "Czech"] 
+
+mldadat[country == "Czech Republic", country := "Czechia"]
+mnames <- unique(mldadat$country)
+
+popdat <- dcast(popdat[country %in% mnames], country + year + pop_all ~ age, value.var = "pop")
+names(popdat) <- c("country","year","pop_all",paste0("pop_",c(16:21)))
+
+data <- merge(mldadat,popdat,
+              by = c("country","year")
+              )
+
+data[, unique(country)]
+
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+## 3) OUTPUT
+# ______________________________________________________________________________________________________________________
+
+saveRDS(data, paste0("output/data_prepared_",DATE,".rds"))
+
+
+
+
+
---
+++ 2_analyse data.R
@@ -0,0 +1,312 @@
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+# PROJECT TITLE:  MLDA paper
+# CODE AUTHOR:    JAKOB
+# DATE STARTED:   260415
+
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+# 0) ESSENTIALS
+# ______________________________________________________________________________________________________________________
+
+# clean workspace
+rm(list=ls())
+
+packages <- c("data.table", "ggplot2", "ggthemes", "openxlsx",
+              "sf","rnaturalearth"
+              #"Hmisc", "tidyr", "nnet",
+              #"tableone", "knitr", "kableExtra",
+              #"survey"
+              )
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+# Load packages
+invisible(lapply(packages, library, character.only = TRUE))
+
+# current date:
+DATE <- format(Sys.Date(), "%y%m%d")
+
+# themes and options
+theme_set( theme_gdocs() + #base_family = "Aptos"
+             theme( panel.background = element_rect(fill = "white") )
+)
+options(scipen = 999)
+
+# fonts:
+extrafont::loadfonts(device = "win")
+
+# viridis colors: https://waldyrious.net/viridis-palette-generator/
+vcol3 <- viridis::viridis(3)
+vcol4 <- viridis::viridis(4)
+vcol5 <- viridis::viridis(5)
+vcol6 <- viridis::viridis(6)
+
+# plasma colors: https://waldyrious.net/viridis-palette-generator/
+pcol <- viridis::plasma(1)
+
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+# 1) LOAD DATA
+# ______________________________________________________________________________________________________________________
+
+##  MLDA data
+# -------------------------------------------------------
+
+path <- file.path("output", "data_prepared_260415.rds")
+input1 <- data.table(readRDS(file = path))
+
+nrow(input1) # 1302
+input1 <- input1[!country == "Greece"]
+
+
+
+
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+## 2) PREPARE DATA
+# ______________________________________________________________________________________________________________________
+
+## 2.1) ...
+#-------------------------------------------------------
+
+
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+## 3) MAP
+# ______________________________________________________________________________________________________________________
+
+europe <- ne_countries(scale = "medium", continent = "Europe", returnclass = "sf")
+
+add <- input1[year == 2020,.(country,premise,MLDA)]
+add[,unique(MLDA), by = country] # DK, FI, SW
+add[, minage := MLDA]
+add[country == "Denmark", minage := "16/18"]
+add[country == "Finland", minage := "18/21"]
+add[country == "Sweden", minage := "18/20"]
+
+add <- unique(add[,.(country,minage = factor(minage))])
+
+europe_data <- merge(europe, add, by.x = "name", by.y = "country", all.x = TRUE)
+europe_data <- europe_data[!is.na(europe_data$minage),]
+
+ggplot(data = europe_data) +
+  geom_sf(aes(fill = minage), color = "gray60", size = 0.12, alpha = 0.9) +
+  scale_fill_manual(
+    values = c(
+      "16" = "#d73027","16/18" = "#fc8d59","18" = "#fee08b",
+      "18/20" = "#d9ef8b","18/21" = "#91bfdb","20" = "#1a9850"),
+    na.value = "lightgray") +
+  coord_sf(xlim = c(-30, 40), ylim = c(35, 73), expand = FALSE) +
+  theme_minimal() +
+  theme(
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    panel.grid = element_blank(),
+    legend.position = "right") +
+  labs(fill = "MLDA")
+
+ggsave("output/main_figure map europe.png", width = 12, height = 6)
+ggsave("output/main_figure map europe.svg", width = 12, height = 6)
+
+
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+# ==================================================================================================================================================================
+
+## 4) Population coverage
+# ______________________________________________________________________________________________________________________
+
+covdat <- copy(input1)
+table(covdat$MLDA)
+covdat[is.na(MLDA), MLDA := 0]
+covdat[,minage := ifelse(any(MLDA == 0),0,
+                         ifelse(any(MLDA == "15"),15,
+                                ifelse(any(MLDA == "16" | MLDA == "16/18"),16,
+                                       ifelse(any(MLDA == "17"),17,
+                                              ifelse(any(MLDA == "18"),18,
+                                                     ifelse(any(MLDA == "18/20" | MLDA == "20"),20,
+                                                            ifelse(any(MLDA == "18/21"),21,NA
+                                                                   ))))))),
+                 by = .(country,year)]
+covdat[is.na(minage)] # none
+covdat <- unique(covdat[,.(country,year,minage,pop_all,pop_16,pop_17,pop_18,pop_19,pop_20,pop_21)])
+
+##  check
+covdat[country == "Croatia"] # 
+covdat[country == "Cyprus"] # 
+covdat[country == "Denmark"] # 
+covdat[country == "France"] # 
+covdat[country == "Italy"] # 
+covdat[country == "Lithuania"] # 
+covdat[country == "Luxembourg"] #
+covdat[country == "Malta"] # 
+covdat[country == "Netherlands"] # 
+covdat[country == "Portugal"] # 
+covdat[country == "Slovenia"] # 
+covdat[country == "Spain"] # 
+
+## 4.1) all ages
+#-------------------------------------------------------
+
+pdat <- copy(covdat); pdat$pop_all <- NULL
+pdat <- melt(pdat, id.vars = c("country","year","minage"), variable.name = "age", value.name = "pop")
+pdat[, age := as.numeric(gsub("pop_","",age))]
+
+pdat[, pop_withaccess := ifelse(minage <=age, pop, 0)]
+
+pdat_agg <- pdat[,.(pop = sum(pop),
+                    pop_withaccess = sum(pop_withaccess)),
+                 by = .(year,age)]
+
+pdat_agg[age == 18]
+pdat[year == 2000 & age == 18, sum(pop)]
+pdat[year == 2000 & age == 18, sum(pop_withaccess)]
+
+ggplot(data = pdat_agg, aes(x = year)) + 
+  ggtitle("Population of Europeans at different ages (green) with legal access to alcoholic beverages (red)" ) + #"Vertical lines indicate year in which countries have increased MLDA from 16 to higher age limit"
+  facet_grid(age ~.) +
+  geom_area(aes(y = pop), fill = "#D8F999") +
+  geom_area(aes(y = pop_withaccess), fill = "#FFCCD3") 
+
+pdat_agg[,.(pop,pop_withaccess,prop = pop_withaccess/pop), by = .(year,age)][year == 2020]
+pdat_agg[,.(pop,pop_withaccess,prop = pop_withaccess/pop), by = .(year,age)][year == 2020 & age <21, .(
+  pop = sum(pop),
+  pop_withaccess = sum(pop_withaccess),
+  prop = sum(pop_withaccess)/sum(pop))]
+pdat_agg[,.(pop,pop_withaccess,prop = pop_withaccess/pop), by = .(year,age)][year == 2000 & age <21, .(
+  pop = sum(pop),
+  pop_withaccess = sum(pop_withaccess),
+  prop = sum(pop_withaccess)/sum(pop))]
+  
+## 4.2) only age16
+#-------------------------------------------------------
+
+p16 <- pdat_agg[age == 16]
+
+ggplot(data = p16, aes(x = year)) + 
+  ggtitle("Population of 16-year-olds (green) with legal access to alcoholic beverages (red)",
+          "Vertical lines indicate year in which countries have increased MLDA affecting 16-year-olds") +
+  geom_area(aes(y = pop), fill = "#D8F999") +
+  geom_area(aes(y = pop_withaccess), fill = "#FFCCD3") + 
+  geom_vline(xintercept = 2003, linetype = 3) + geom_label(x = 2003, y = 4.5, label = "2003\nCroatia") +
+  geom_vline(xintercept = 2009, linetype = 3) + geom_label(x = 2009, y = 4.5, label = "2009\nMalta") +
+  geom_vline(xintercept = 2010, linetype = 3) + geom_label(x = 2010, y = 3.5, label = "2010\nFrance") +
+  geom_vline(xintercept = 2013, linetype = 3) + geom_label(x = 2013, y = 4.5, label = "2013\nItaly") +
+  geom_vline(xintercept = 2014, linetype = 3) + geom_label(x = 2014, y = 3.5, label = "2014\nNetherlands") +
+  geom_vline(xintercept = 2016, linetype = 3) + geom_label(x = 2016, y = 4.5, label = "2016\nPortugal and Spain") + 
+  scale_x_continuous("", breaks = seq(2000,2020,2)) + 
+  scale_y_continuous("Population of 16-year-olds in 30 European countries (in millions)", expand = F) +
+  theme(panel.grid.minor = element_line(color = "gray80",linetype = "dotted"),
+        axis.text.x = element_text(color = "black"), # family="Aptos",
+        axis.text.y = element_text(color = "black"), # family="Aptos",
+        axis.title.x = element_text(color = "black"),
+        axis.title.y = element_text(color = "black"),
+        plot.title = element_text(color = "black"),
+        plot.subtitle = element_text(color = "black"))
+
+ggsave("output/main_figure coverage 16yo.png", width = 12, height = 6)
+ggsave("output/main_figure coverage 16yo.svg", width = 12, height = 6)
+
+p16[,.(year,pop,pop_withaccess,prop = pop_withaccess/pop)]
+
+
+## 4.3) only age17
+#-------------------------------------------------------
+
+p17 <- pdat_agg[age == 17]
+
+ggplot(data = p17, aes(x = year)) + 
+  ggtitle("Population of 17-year-olds (green) with legal access to alcoholic beverages (red)",
+          "Vertical lines indicate year in which countries have increased MLDA affecting 17-year-olds") +
+  geom_area(aes(y = pop), fill = "#D8F999") +
+  geom_area(aes(y = pop_withaccess), fill = "#FFCCD3") + 
+  geom_vline(xintercept = 2003, linetype = 3) + geom_label(x = 2003, y = 4.5, label = "2003\nCroatia") +
+  geom_vline(xintercept = 2010, linetype = 3) + geom_label(x = 2010, y = 3.5, label = "2010\nFrance") +
+  geom_vline(xintercept = 2013, linetype = 3) + geom_label(x = 2013, y = 4.5, label = "2013\nItaly") +
+  geom_vline(xintercept = 2014, linetype = 3) + geom_label(x = 2014, y = 3.5, label = "2014\nNetherlands") +
+  geom_vline(xintercept = 2016, linetype = 3) + geom_label(x = 2016, y = 4.5, label = "2016\nPortugal and Spain") + 
+  scale_x_continuous("", breaks = seq(2000,2020,2)) + 
+  scale_y_continuous("Population of 17-year-olds in 30 European countries (in millions)", expand = F) +
+  theme(panel.grid.minor = element_line(color = "gray80",linetype = "dotted"),
+        axis.text.x = element_text(color = "black"), # family="Aptos",
+        axis.text.y = element_text(color = "black"), # family="Aptos",
+        axis.title.x = element_text(color = "black"),
+        axis.title.y = element_text(color = "black"),
+        plot.title = element_text(color = "black"),
+        plot.subtitle = element_text(color = "black"))
+
+ggsave("output/suppl_figure coverage 17yo.png", width = 12, height = 6)
+ggsave("output/suppl_figure coverage 17yo.svg", width = 12, height = 6)
+
+p17[,.(year,pop,pop_withaccess,prop = pop_withaccess/pop)]
+
+
+## 4.3) only age18
+#-------------------------------------------------------
+
+p18 <- pdat_agg[age == 18]
+
+ggplot(data = p18, aes(x = year)) + 
+  ggtitle("Population of 18-year-olds (green) with legal access to alcoholic beverages (red)",
+          "Vertical lines indicate year in which countries have increased MLDA affecting 18-year-olds") +
+  geom_area(aes(y = pop), fill = "#D8F999") +
+  geom_area(aes(y = pop_withaccess), fill = "#FFCCD3") + 
+  geom_vline(xintercept = 2018, linetype = 3) + geom_label(x = 2018, y = 4.5, label = "2018\nLithuania") +
+  scale_x_continuous("", breaks = seq(2000,2020,2)) + 
+  scale_y_continuous("Population of 18-year-olds in 30 European countries (in millions)", expand = F) +
+  theme(panel.grid.minor = element_line(color = "gray80",linetype = "dotted"),
+        axis.text.x = element_text(color = "black"), # family="Aptos",
+        axis.text.y = element_text(color = "black"), # family="Aptos",
+        axis.title.x = element_text(color = "black"),
+        axis.title.y = element_text(color = "black"),
+        plot.title = element_text(color = "black"),
+        plot.subtitle = element_text(color = "black"))
+
+ggsave("output/suppl_figure coverage 18yo.png", width = 12, height = 6)
+ggsave("output/suppl_figure coverage 18yo.svg", width = 12, height = 6)
+
+p18[,.(year,pop,pop_withaccess,prop = pop_withaccess/pop)]
+
+
+## 4.3) only age19
+#-------------------------------------------------------
+
+p19 <- pdat_agg[age == 19]
+
+ggplot(data = p19, aes(x = year)) + 
+  ggtitle("Population of 19-year-olds (green) with legal access to alcoholic beverages (red)",
+          "Vertical lines indicate year in which countries have increased MLDA affecting 19-year-olds") +
+  geom_area(aes(y = pop), fill = "#D8F999") +
+  geom_area(aes(y = pop_withaccess), fill = "#FFCCD3") + 
+  geom_vline(xintercept = 2019, linetype = 3) + geom_label(x = 2019, y = 4.5, label = "2019\nLithuania") +
+  scale_x_continuous("", breaks = seq(2000,2020,2)) + 
+  scale_y_continuous("Population of 19-year-olds in 30 European countries (in millions)", expand = F) +
+  theme(panel.grid.minor = element_line(color = "gray80",linetype = "dotted"),
+        axis.text.x = element_text(color = "black"), # family="Aptos",
+        axis.text.y = element_text(color = "black"), # family="Aptos",
+        axis.title.x = element_text(color = "black"),
+        axis.title.y = element_text(color = "black"),
+        plot.title = element_text(color = "black"),
+        plot.subtitle = element_text(color = "black"))
+
+ggsave("output/suppl_figure coverage 19yo.png", width = 12, height = 6)
+ggsave("output/suppl_figure coverage 19yo.svg", width = 12, height = 6)
+
+p19[,.(year,pop,pop_withaccess,prop = pop_withaccess/pop)]
+
+
+
